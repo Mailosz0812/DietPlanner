380a205ecc5471ddd175ab22a34f2244a88cf930
Obvinimus <obvinimus@protonmail.com>
2024-12-18 00:48:08 +0100

    Znowu zmiany do GUI

--- DietPlanner1/src/main/java/org/locations/dietplanner/HelloApplication.java
+++ DietPlanner1/src/main/java/org/locations/dietplanner/HelloApplication.java
@@ -2,10 +2,7 @@ package org.locations.dietplanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
