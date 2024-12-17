9016150df1a3a8ea48280afda047d753d5b2310f
Obvinimus <obvinimus@protonmail.com>
2024-12-17 22:32:51 +0100

    Delikatnie zmienione UI

--- DietPlanner1/src/main/java/org/locations/dietplanner/HelloApplication.java
+++ DietPlanner1/src/main/java/org/locations/dietplanner/HelloApplication.java
@@ -3,6 +3,7 @@ package org.locations.dietplanner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("index.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),784,505 );
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        stage.setTitle("Test");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
