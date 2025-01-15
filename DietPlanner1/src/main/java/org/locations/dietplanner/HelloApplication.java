9499c187f4426fbf3bfb512b8a210f4064e897f0
Obvinimus <obvinimus@protonmail.com>
2025-01-15 23:39:50 +0100

    jeszcze raz rodzialka

--- DietPlanner1/src/main/java/org/locations/dietplanner/HelloApplication.java
+++ DietPlanner1/src/main/java/org/locations/dietplanner/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("index.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),1600,800 );
+        Scene scene = new Scene(fxmlLoader.load(),1200,700 );
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle("Diet App");
         stage.setScene(scene);
