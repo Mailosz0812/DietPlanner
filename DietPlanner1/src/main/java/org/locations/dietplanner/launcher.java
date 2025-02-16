fbbf37a11c405e1afea380e1e878200275a896cf
Mailosz0812 <89217@student.pb.edu.pl>
2025-02-16 22:03:46 +0100

    Final commit
    Jar and exe files made

---
+++ DietPlanner1/src/main/java/org/locations/dietplanner/launcher.java
@@ -0,0 +1,9 @@
+package org.locations.dietplanner;
+
+import javafx.application.Application;
+
+public class launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class,args);
+    }
+}
